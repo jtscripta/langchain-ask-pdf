b05610453e6043b5a9cd01f0aa770cd8b7d6d4f3
jtscripta <87989965+jtscripta@users.noreply.github.com>
2023-08-24 15:39:56 -0400

    styling, refactoring

--- app.py
+++ app.py
@@ -12,55 +12,36 @@ from langchain.callbacks import get_openai_callback
 import os
 
 st.set_page_config(page_title="Ask your PDF")
-
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-
 langchain.llm_cache = InMemoryCache()
 
+models_url = "https://platform.openai.com/docs/models"
+temp_url = "https://www.linkedin.com/pulse/temperature-check-guide-best-chatgpt-feature-youre-using-berkowitz"
 
-def main():
-    print(st.secrets)
-    load_dotenv()
-    st.header("Ask your PDF 💬")
 
-    # upload file
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
+def extract_text_from_pdf(pdf):
+    pdf_reader = PdfReader(pdf)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
 
-    # extract the text
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
 
-        # split into chunks
-        text_splitter = CharacterTextSplitter(
-            separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
-        )
-        chunks = text_splitter.split_text(text)
+def create_embeddings(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(text_chunks, embeddings)
+    return knowledge_base
 
-        # create embeddings
-        embeddings = OpenAIEmbeddings()
-        knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-        # settings
-        col1, col2 = st.columns(2)
-        with col1:
-            models_url = "https://platform.openai.com/docs/models"
-            model = st.selectbox(
-                "Choose the model ([learn more](%s))" % models_url,
-                ("gpt-3.5-turbo-16k", "gpt-3.5-turbo", "text-davinci-003", "gpt-4"),
-            )
-        with col2:
-            temp_url = "https://www.linkedin.com/pulse/temperature-check-guide-best-chatgpt-feature-youre-using-berkowitz"
-            temp = st.text_input(
-                "Temperature (0.1 - 1.0 [learn more](%s))" % temp_url, 0.1
-            )
+def user_input_section(knowledge_base, model, temp):
+    # user_question = st.text_input("Ask a question about your PDF:")
+    user_question = st.chat_input("Ask a question about your PDF:")
 
-        # show user input
-        st.write(" ")
-        user_question = st.text_input("Ask a question about your PDF:")
-        if user_question:
+    if user_question:
+        with st.chat_message("user"):
+            st.write(user_question)
+
+        with st.spinner(""):
             docs = knowledge_base.similarity_search(user_question)
 
             llm = OpenAI(temperature=float(temp), model_name=model)
@@ -69,16 +50,53 @@ def main():
                 response = chain.run(input_documents=docs, question=user_question)
                 print(cb)
                 print(response)
+            with st.chat_message("assistant"):
+                st.write(response)
+                st.write(" ")
+                st.write(cb)
+                st.markdown(
+                    f"<span style='color: gray; font-size: 11px;'>Model: {model}</span>",
+                    unsafe_allow_html=True,
+                )
 
-            st.write(response)
 
-            # response detail
-            st.write(" ")
-            st.write(cb)
-            st.markdown(
-                f"<span style='color: gray; font-size: 11px;'>Model: {model}</span>",
-                unsafe_allow_html=True,
+def main():
+    load_dotenv()
+    st.header("Ask your PDF 💬")
+
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+    # extract the text
+    if pdf is not None:
+        text = extract_text_from_pdf(pdf)
+
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+
+        print("Embedding to run: ...")
+
+        # create embeddings
+        knowledge_base = create_embeddings(chunks)
+
+        # settings
+        col1, col2 = st.columns(2)
+        with col1:
+            model = st.selectbox(
+                "Choose the model ([learn more](%s))" % models_url,
+                ("gpt-3.5-turbo-16k", "gpt-3.5-turbo", "text-davinci-003", "gpt-4"),
             )
+        with col2:
+            temp = st.text_input(
+                "Temperature (0.1 - 1.0 [learn more](%s))" % temp_url, 0.1
+            )
+
+        # show user input
+        st.divider()
+        user_input_section(knowledge_base, model, temp)
 
 
 if __name__ == "__main__":
